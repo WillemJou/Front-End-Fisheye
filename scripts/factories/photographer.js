53a4d5dea0958ce903c0965c4216705aa46cabf6
Willem Jouret <willem.jouret9@gmail.com>
2022-05-17 18:51:43 +0200

    récupération data & début intégration de la homepage via JS et base de données

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,28 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price} = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const article = document.createElement( 'article' );
+    const img = document.createElement( 'img' );
+    const pTagline = document.createElement('p');
+    const pPrice = document.createElement('p');
+    const h2 = document.createElement( 'h2' );
+    const h3 = document.createElement( 'h3' );
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        h2.textContent = name;       
+        h3.textContent = city, country; 
+        pTagline.className = 'p-tagline-homepage';
+        pTagline.textContent = tagline;  
+        pPrice.className = 'p-price-homepage'; 
+        pPrice.textContent = price;
+        pPrice.innerHTML = "/jours";
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,74 @@
-    async function getPhotographers() {
+    
+     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
+          /*const photographers = {
+        await fetch('../../data/photographers.json')
+         .then(res => res.json())
+         .then((dataPhotographers) => { 
+            array = dataPhotographers;
+            console.log(array);
+        })}
+        
+        return photographers; */ 
+        const photographers =  [
             {
-                "name": "Ma data test",
-                "id": 1,
+                "name": "Mimi Keel",
+                "id": 243,
+                "city": "London",
+                "country": "UK",
+                "tagline": "Voir le beau dans le quotidien",
+                "price": 400,
+                "portrait": "Portrait_Nora.jpg"
+            },
+            {
+                "name": "Ellie-Rose Wilkens",
+                "id": 930,
                 "city": "Paris",
                 "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
+                "tagline": "Capturer des compositions complexes",
+                "price": 250,
+                "portrait": "Architecture_Horseshoe.jpg"
             },
             {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
+                "name": "Tracy Galindo",
+                "id": 82,
+                "city": "Montreal",
+                "country": "Canada",
+                "tagline": "Photographe freelance",
                 "price": 500,
-                "portrait": "account.png"
+                "portrait": "Fashion_Urban_Jungle.jpg"
             },
+            {
+                "name": "Nabeel Bradford",
+                "id": 527,
+                "city": "Mexico City",
+                "country": "Mexico",
+                "tagline": "Toujours aller de l'avant",
+                "price": 350,
+                "portrait": "Travel_Outdoor_Baths.jpg"
+            },
+            {
+                "name": "Rhode Dubois",
+                "id": 925,
+                "city": "Barcelona",
+                "country": "Spain",
+                "tagline": "Je crée des souvenirs",
+                "price": 275,
+                "portrait": "Fashion_Melody_Red_on_Stripes.jpg"
+            },
+            {
+                "name": "Marcel Nikolic",
+                "id": 195,
+                "city": "Berlin",
+                "country": "Germany",
+                "tagline": "Toujours à la recherche de LA photo",
+                "price": 300,
+                "portrait": "Travel_Tower.jpg"
+            }
         ]
         // et bien retourner le tableau photographers seulement une fois
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
 
     async function displayData(photographers) {
@@ -42,4 +88,5 @@
     };
     
     init();
-    
+        
+ 
